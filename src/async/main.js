c68df0841741d12a5bbf5585173c2c6c09718dce
jcasadiego <jcasadiego.a@gmail.com>
2022-05-19 09:06:52 -0400

    add button y fuction async/await

--- src/async/main.js
+++ src/async/main.js
@@ -8,10 +8,16 @@ const fetchFunction = async (url_api) => {
         const data = await res.json();
         const img = document.querySelector('img');
         img.src = data[0].url;
+        console.log('Se realizo la peticion exitosamente');
 
     } catch (error) {
         console.log(error);
     }
 }
 
+const myButton = document.querySelector("button");
+myButton.onclick = fetchFunction;
+
+console.log('Before');
 fetchFunction(API);
+console.log('After');
